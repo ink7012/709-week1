be34b37d38e47ad715b777e8ea5e43489edecad2
ink7012 <pchu5@student.touro.edu>
2023-09-10 18:26:54 -0400

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -6,20 +6,25 @@ public class Guess
 {
   public static void main(String[] args)
   {
+    //intiialized Scanner class and variables
     Scanner in = new Scanner(System.in);
     Random random = new Random();
 
     int answer = random.nextInt(100) + 1;
     int guess;
     int difference;
-  
-    System.out.print("I'm thinking of a number between 1 and 100       (including both). Can you guess what it is?\nType a number:         ");
+    
+    //asks user for input
+    System.out.print("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?\n Type a number: ");
 
+    //assigning guess and difference variable
     guess = in.nextInt();
     difference = Math.abs(guess - answer);
 
+
+    //Tells user their guess, the answer, and the difference.
     System.out.println("Your guess is: " + guess);
-    System.out.println("The number I was thinking of is: " +            answer + "\nYou were off by: " + difference);
+    System.out.println("The number I was thinking of is: " + answer + "\nYou were off by: " + difference);
   }
 }
 
